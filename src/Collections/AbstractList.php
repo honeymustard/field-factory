57fac82689205153278c0550a1759101d7c3c7ff
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2017-09-10 14:59:13 +0200

    makes abstract list iterable

--- src/Collections/AbstractList.php
+++ src/Collections/AbstractList.php
@@ -5,12 +5,82 @@ namespace Honeymustard\FieldFactory\Collections;
 /**
  * Base class for all lists.
  */
-abstract class AbstractList
+abstract class AbstractList implements \Iterator
 {
+    private $list = [];
+    private $position = 0;
+
     /**
      * Get the length of the list.
      *
      * @return int
      */
-    abstract public function length();
+    public function length() {
+        return count($this->list);
+    }
+
+    /**
+     * Reset the iteration.
+     *
+     * @return void
+     */
+    public function rewind() {
+        $this->position = 0;
+    }
+
+    /**
+     * Get the current value.
+     *
+     * @return mixed
+     */
+    public function current() {
+        return $this->list[$this->position];
+    }
+
+    /**
+     * Get the current index.
+     *
+     * @return int
+     */
+    public function key() {
+        return $this->position;
+    }
+
+    /**
+     * Set the next position.
+     *
+     * @return void
+     */
+    public function next() {
+        ++$this->position;
+    }
+
+    /**
+     * Determine if current position is valid.
+     *
+     * @return boolean
+     */
+    public function valid() {
+        return isset($this->list[$this->position]);
+    }
+
+    /**
+     * Add an item to the list.
+     *
+     * @param mixed $item
+     *
+     * @return boolean
+     */
+    public function setItem($item) {
+        $this->list[] = $item;
+    }
+
+    /**
+     * Get the inner array structure.
+     *
+     * @return mixed[]
+     */
+    public function toArray() {
+        return $this->list;
+    }
 }
