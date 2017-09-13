de075ca10c010e81ffa1be26f25d0443ecb1dc5d
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2017-09-13 20:21:24 +0200

    adds basic concrete layout

---
+++ src/Layouts/Layout.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Honeymustard\FieldFactory\Layouts;
+
+use Honeymustard\FieldFactory\Factory;
+
+/**
+ * Concrete layout implementation.
+ */
+final class Layout extends AbstractLayout
+{
+    /**
+     * Construct a new layout.
+     *
+     * @param string[] $args A list of arguments.
+     */
+    public function __construct($args)
+    {
+        parent::__construct($args);
+    }
+}
