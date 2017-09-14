0687bd3f86b39d732593955f4a5e8fe75e049284
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2017-09-14 21:46:21 +0200

    adds ability to merge and parse field arguments

---
+++ src/Abilities/Mergable.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Honeymustard\FieldFactory\Abilities;
+
+/**
+ * Enables an object to merge field arguments.
+ */
+trait Mergable
+{
+    /**
+     * Verify a list of arguments.
+     *
+     * @param string[] $args List of arguments.
+     *
+     * @return string[]
+     */
+    abstract protected function verify($args);
+
+    /**
+     * Get the translator.
+     *
+     * @return Translator
+     */
+    abstract protected function getTranslator();
+
+    /**
+     * Parse a list of argument lists.
+     *
+     * @param string[][] $args List of argument lists.
+     *
+     * @return string[]
+     */
+    public function parse($args = [])
+    {
+        if (count($args) <= 1) {
+            return $this->verify($args);
+        } else {
+            return $this->verify($this->combine($args));
+        }
+    }
+
+    /**
+     * Recursively combine a list of argument lists.
+     *
+     * @param string[][] $lists List of argument lists.
+     *
+     * @return string[]
+     */
+    protected function combine($lists)
+    {
+        $length = count($lists);
+        $head = $lists[0];
+
+        if ($length === 1) {
+            return $head;
+        } else {
+            $tail = array_slice($lists, 1, $length);
+            return $this->merge($head, $this->combine($tail));
+        }
+    }
+
+    /**
+     * Merge two argument lists.
+     *
+     * @param string[] $a List of arguments.
+     * @param string[] $b List of arguments.
+     *
+     * @return string[]
+     */
+    protected function merge($a, $b)
+    {
+        return array_merge($this->translate($a), $this->translate($b));
+    }
+
+    /**
+     * Translate a list of arguments.
+     *
+     * @param string[] $args List of arguments.
+     *
+     * @return string[]
+     */
+    protected function translate($args)
+    {
+        return $this->getTranslator()->translate($args);
+    }
+}
