8ea0934d33685b80ce488b9f820da96240b7defd
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2017-09-16 13:48:49 +0200

    adds converter utility class

---
+++ src/Utils/Converter.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Honeymustard\FieldFactory\Utils;
+
+/**
+ * Generic conversion methods.
+ */
+final class Converter
+{
+    /**
+     * Recursively apply toArray on a list.
+     *
+     * @param $list A list.
+     *
+     * @return string[]
+     */
+    public static function toArray($list)
+    {
+        $temp = [];
+
+        if (!empty($list)) {
+            foreach ($list as $k => $v) {
+                if (is_array($v)) {
+                    $temp[$k] = self::toArray($v);
+                } else if(is_object($v)) {
+                    $temp[$k] = self::toArray($v->toArray());
+                } else {
+                    $temp[$k] = $v;
+                }
+            }
+        }
+
+        return $temp;
+    }
+}
