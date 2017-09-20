60950ef9c26496f21378f517a2e482b7690b6d28
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2017-09-20 15:03:15 +0200

    adds arrayable interface

---
+++ src/Interfaces/ArrayableInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Honeymustard\FieldFactory\Interfaces;
+
+/**
+ * An interface for objects that give data as an array.
+ */
+interface ArrayableInterface
+{
+    public function toArray();
+}
